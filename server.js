6d79a76da38196ff666d7abf765e160389b78587
Manoj S M <manojsm293@gmail.com>
2020-01-03 23:40:54 +0530

    Minor update to package json for deployment

--- server.js
+++ server.js
@@ -68,10 +68,10 @@ app.use(
 );
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('public'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
   });
 }
 
