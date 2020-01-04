5f78584e4aa2a24a8e00a1c131307c8712b76ba3
Manoj S M <manojsm293@gmail.com>
2020-01-04 13:22:36 +0530

    'minor_fix'

--- client/src/index.js
+++ client/src/index.js
@@ -24,7 +24,7 @@ import {
 // connecting the front-end to the back-end
 
 const client = new ApolloClient({
-  uri: 'https://gentle-hollows-45761.herokuapp.com/graphql',
+  uri: 'https://gentle-hollows-45761.herokuapp.com/',
   fetchOptions: {
     credentials: 'include',
   },
